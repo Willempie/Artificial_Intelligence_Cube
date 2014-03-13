f69d8247bd06ae2a9084fd7724536689460b7ea3
Willempie <willem_dijkstra@live.nl>
2014-03-13 11:06:39 +0100

    GUI

--- my_visual/my_display.py
+++ my_visual/my_display.py
@@ -4,10 +4,20 @@ from my_box import MyBox
 
 
 class MyDisplay(display):
+    L = 320  # length for window
+    d = 20
 
     def __init__(self):
         display.__init__(self)
-        self.lights = []
+
+        #initialize gui
+        w = window(width=2*(self.L+window.dwidth), height=self.L+window.dheight+window.menuheight, menus=True,
+                   title='Artificial Intelligence Cube')
+
+        #display for the cube
+        display(window=w, x=self.d, y=self.d, width=self.L-2*self.d, height=self.L-2*self.d, forward=-vector(0, 1, 2))
+
+        self.lights = [1]
         self.ambient = color.gray(1)
 
         my_cube = MyCube(3, 2)
