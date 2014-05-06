2bb31dcbda7b2e38eb43f2db03b99a7c21a5ed2d
Willempie <willem_dijkstra@live.nl>
2014-05-06 11:42:35 +0200

    Filedialog

--- logic/handling/files.py
+++ logic/handling/files.py
@@ -1,3 +1,4 @@
+import os
 import wx
 
 __author__ = 'Willem'
@@ -6,6 +7,7 @@ __author__ = 'Willem'
 class HandleFiles():
 
     __window = None
+    __file_location = None
 
     def __init__(self, window):
         self.__window = window
@@ -14,12 +16,24 @@ class HandleFiles():
     def open(self, event):
         openFileDialog = wx.FileDialog(self.__window, "Open XML file", "", "",
                                        "XML files (*.xml)|*.xml", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
-        openFileDialog.ShowModal()
-        pass
+        if openFileDialog.ShowModal() == wx.ID_OK:
+            filename = openFileDialog.GetFilename()
+            directory = openFileDialog.GetDirectory()
+            self.__file_location = directory + "/" + filename
+            return self.__file_location
+        openFileDialog.Destroy()
 
     def save(self, event):
-        print("save")
-        pass
+        saveFileDialog = wx.FileDialog(self.__window, "Save XML file", "", "",
+                                       "XML files (*.xml)|*.xml", wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
+
+        if saveFileDialog.ShowModal() == wx.ID_OK:
+            filename = saveFileDialog.GetFilename()
+            directory = saveFileDialog.GetDirectory()
+
+            self.__file_location = directory + "/" + filename
+            return self.__file_location
+        saveFileDialog.Destroy()
 
     def close(self, event):
         pass
